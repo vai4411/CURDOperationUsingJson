df6eb0096ce62ab3941b90075bcb5ee08d4ebbb2
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-24 14:42:56 +0530

    ADDED: Used Person Model For Storing Data

---
+++ src/main/java/com/bl/crudoperation/model/Person.java
@@ -0,0 +1,48 @@
+package com.bl.crudoperation.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    private long contactNumber;
+    private String time;
+}
